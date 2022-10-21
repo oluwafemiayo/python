1afe44c03c111856ca643a284245163c653cc7b2
oluwafemi <oluyodele123@gmail.com>
2022-10-22 00:00:54 +0300

    first commit

---
+++ project.py
@@ -0,0 +1,12 @@
+print ("welcome to my website")
+
+age = int(input("How are are you please?"))
+
+if age >= 21:
+    print ("Welcome to the club")
+elif age >= 18:
+    print ("You need to be at least 21 years old")
+
+else:
+    print("youre a kid, get out.")
+
---
+++ variables.py
@@ -0,0 +1,11 @@
+box = "chocolate cake"
+print (box)
+
+phone = ("08136378860")
+print (phone)
+
+rain = False
+sun = True
+
+print (rain)
+print (sun)
